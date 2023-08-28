ac8c3b4da54e85ec66408de989495b093df4ec96
joseLuís <joseluis@andamira.net>
2023-08-28 14:24:23 +0200

    overhaul features
    - remove `safe` from default features.
    - rename `no-std` feature to `no_std`
    - new features: `nightly_docs`, `unsafest`.
    - update `devela`.
    - deprecate `no-std`.
    - refactor manifest.
    - update categories.
    - update dependencies.
    - update docs.
    - update CI.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 [![Crate](https://img.shields.io/crates/v/ladata.svg)](https://crates.io/crates/ladata)
 [![API](https://docs.rs/ladata/badge.svg)](https://docs.rs/ladata/)
+[![MSRV: 1.72.0](https://flat.badgen.net/badge/MSRV/1.72.0/purple)](https://releases.rs/docs/1.72.0/)
 
-A simple & modular mixed data model.
+A comprehensive system of data structures.
 
 See [the documentation](https://docs.rs/ladata/) for more information.
 
@@ -11,8 +12,16 @@ See [the documentation](https://docs.rs/ladata/) for more information.
 
 This is currently in an experimental stage of development.
 
+## License
+This project is dual licensed under either [MIT](LICENSE-MIT)
+or [Apache-2.0](LICENSE-APACHE) at your option.
+
 ## Contributing
 
 Contributions are welcomed to help refine and improve this library over time.
 If you notice a bug, have an idea for a new feature, or simply want to suggest
 improvements to the existing codebase, please get in touch.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by shall be dual licensed as above,
+without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
 // ladata::lib
+//
 #![doc = include_str!("./Lib.md")]
 //
+
+// warnings
 #![warn(clippy::all)]
 #![allow(non_snake_case, non_camel_case_types, clippy::module_inception)]
-//
+#![allow(unused_attributes, unused_doc_comments)] // needed in libera
+// environment
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(feature = "safe", forbid(unsafe_code))]
 #![cfg_attr(feature = "nightly", feature(doc_cfg))]
+#[cfg(feature = "alloc")]
+extern crate alloc;
 
+// safeguards
 #[cfg(all(feature = "std", feature = "no-std"))]
 compile_error!("You can't enable the `std` and `no-std` features at the same time.");
 #[cfg(all(
@@ -15,9 +22,8 @@ compile_error!("You can't enable the `std` and `no-std` features at the same tim
     any(feature = "unsafe", feature = "unsafe_constructors",)
 ))]
 compile_error!("You can't enable the `safe` and `unsafe*` features at the same time.");
-
-#[cfg(feature = "alloc")]
-extern crate alloc;
+// deprecated
+devela::deprecate_feature![old: "no-std", new: "no_std", since: "0.8.0"];
 
 pub mod error;
 pub mod grid;
@@ -29,9 +35,7 @@ pub mod misc;
 // pub mod tree;
 pub mod unit;
 
-/// All types.
-///
-/// Everything is re-exported from here.
+/// All items are reexported here.
 pub mod all {
     #[doc(inline)]
     pub use super::{error::*, grid::*, list::all::*, mem::all::*, misc::*, unit::all::*};
